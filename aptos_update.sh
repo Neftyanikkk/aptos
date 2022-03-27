48a225648ff01d4ce969f732b1f7eb8c62c4b3c8
Roman <96354836+Zhoas@users.noreply.github.com>
2022-03-27 05:15:18 +0400

    Create aptos_update.sh

---
+++ aptos_update.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+echo "=================================================="
+curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 2
+echo "=================================================="
+
+sleep 2
+
+echo -e "\e[1m\e[32m1. Stopping the FullNode \e[0m" && sleep 1
+
+cd $HOME/aptos
+sudo docker compose down -v
+sudo docker pull aptoslab/validator:devnet
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32m2. Downloading new genesis and waypoint configs \e[0m" && sleep 1
+
+rm $HOME/aptos/genesis.blob
+wget -P $HOME/aptos https://devnet.aptoslabs.com/genesis.blob
+
+rm $HOME/aptos/waypoint.txt
+wget -P $HOME/aptos https://devnet.aptoslabs.com/waypoint.txt
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32m3. Setting a new waypoint param in the public_full_node.yaml file \e[0m" && sleep 1
+
+sed -i.bak 's/\(from_config: \).*/\1"'$(cat $HOME/aptos/waypoint.txt)'"/g' $HOME/aptos/public_full_node.yaml
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32m4. Restarting the node \e[0m" && sleep 1
+
+sudo docker compose up -d
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32m5. Aptos FullNode Started \e[0m"
+
+echo "=================================================="
+
+echo -e "\e[1m\e[32mTo check sync status: \e[0m" 
+echo -e "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m" 
+
+echo -e "\e[1m\e[32mTo view logs: \e[0m" 
+echo -e "\e[1m\e[39m    docker logs -f aptos-fullnode-1 --tail 5000 \n \e[0m" 
+
+echo -e "\e[1m\e[32mTo stop: \e[0m" 
+echo -e "\e[1m\e[39m    docker compose stop \n \e[0m" 
