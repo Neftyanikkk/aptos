cae62581d47c8922b338ef542c7ca9e02938b5c1
Roman <96354836+Zhoas@users.noreply.github.com>
2022-03-29 13:58:13 +0400

    Update aptos_update.sh

--- aptos_update.sh
+++ aptos_update.sh
@@ -6,7 +6,7 @@ cd $HOME/aptos
 sudo docker compose down -v
 sudo docker pull aptoslab/validator:devnet
 
-echo "=================================================="
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
 
 echo -e "\e[1m\e[32m2. Downloading \e[0m" && sleep 1
 
@@ -16,23 +16,23 @@ wget -P $HOME/aptos https://devnet.aptoslabs.com/genesis.blob
 rm $HOME/aptos/waypoint.txt
 wget -P $HOME/aptos https://devnet.aptoslabs.com/waypoint.txt
 
-echo "=================================================="
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
 
 echo -e "\e[1m\e[32m3. Setting a new waypoint param in the public_full_node.yaml file \e[0m" && sleep 1
 
 sed -i.bak 's/\(from_config: \).*/\1"'$(cat $HOME/aptos/waypoint.txt)'"/g' $HOME/aptos/public_full_node.yaml
 
-echo "=================================================="
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
 
 echo -e "\e[1m\e[32m4. Restarting the node \e[0m" && sleep 1
 
 sudo docker compose up -d
 
-echo "=================================================="
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
 
 echo -e "\e[1m\e[32m5. Aptos FullNode Started \e[0m"
 
-echo "=================================================="
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
 
 echo -e "\e[1m\e[32mTo check sync status: \e[0m" 
 echo -e "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m" 
